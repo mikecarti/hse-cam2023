d2e9d6b6c2432c29a10128c69a39dbc9db045618
Daniel Timchenko <mikecartibeats@gmail.com>
2024-02-15 20:38:44 +0200

    docs

--- coordinate_transform/transform.py
+++ coordinate_transform/transform.py
@@ -13,7 +13,8 @@ class CoordinateTransform:
     Args:
         stadium_length (int): The length of the stadium in meters.
         stadium_width (int): The width of the stadium in meters.
-        corner_src_points (List[Tuple[int, int]]): A list of tuples, where each tuple represents the coordinates of one of the four corners of the original plane.
+        corner_src_points (List[Tuple[int, int]]): A list of tuples, where each tuple represents the coordinates of one
+                                                        of the four corners of the original plane.
 
     Attributes:
         stadium_ratio (float): The ratio of the stadium's length to its width.
@@ -32,6 +33,65 @@ class CoordinateTransform:
         self.default_tracking_players_path = "coordinate_transform/data/yantar-230722-02_track.csv"
         self.MAX_FRAMES = 20000
         self.center_orig_perspective = [1846, 343]  # 1846, 343 - are approximately the center of the stadium left side
+
+    def transform_coordinates(self, track_boxes_df: pd.DataFrame = None,
+                              file_save_name: str = 'track_df_new_coords.csv') -> pd.DataFrame:
+        """
+        Transforms the coordinates of the players in the given DataFrame from the original perspective to the top perspective.
+
+        Args:
+            track_boxes_df (pd.DataFrame, optional): The DataFrame containing the tracking data. If None, the default tracking data will be used.
+            file_save_name (str, optional): The name of the file to which the transformed coordinates will be saved. Defaults to 'track_df_new_coords.csv'.
+
+        Returns:
+            pd.DataFrame: The transformed DataFrame.
+        """
+
+        # transform pos person -> pos trans
+        # 25 FPS
+        M = self._get_projective_transform_matrix()
+
+        if track_boxes_df is None:
+            track_boxes_df = pd.read_csv(self.default_tracking_players_path)
+
+        shift = M @ np.array([0, 0, 1])
+        track_boxes_df = self._downscale_df(track_boxes_df)
+        tracking_coords_transformed = []
+
+        for i in range(1, track_boxes_df["frame"].max() + 1):
+            if i > self.MAX_FRAMES:
+                logger.info(f"Max frames reached. Stopping: {self.MAX_FRAMES}")
+                break
+            logger.info(f"Transforming frame {i}...")
+            df = track_boxes_df[track_boxes_df["frame"] == i]
+            tracking_coords_transformed += self._get_position_transformed(df, transformation_matrix=M)
+
+        transformed_coords = pd.DataFrame(tracking_coords_transformed, columns=['id', 'x', 'y', 'coef'])
+        transformed_coords['frame'] = track_boxes_df['frame']
+        transformed_coords['x'] -= shift[0]
+        transformed_coords['y'] -= shift[1]
+        transformed_coords = transformed_coords[['frame', 'id', 'x', 'y', 'coef']]
+        transformed_coords.to_csv(file_save_name)
+        return transformed_coords
+
+    def get_top_view_center(self) -> np.ndarray:
+        M = self._get_projective_transform_matrix()
+        center = M @ np.array(self.center_orig_perspective + [1])
+        return center
+
+    def image_to_top_perspective(self, image: np.ndarray) -> np.ndarray:
+        """
+        Transforms an image from the original perspective to the top perspective.
+
+        Args:
+            image (np.ndarray): The image to be transformed.
+
+        Returns:
+            np.ndarray: The transformed image.
+        """
+        height, width = image.shape[:2]
+        M = self._get_projective_transform_matrix()
+        return cv2.warpPerspective(image, M, (width, height), borderValue=(255, 255, 255))
 
     def _init_destination_points(self):
         """
@@ -60,20 +120,6 @@ class CoordinateTransform:
         dest = np.float32(self.corner_dest_points)
         M = cv2.getPerspectiveTransform(source, dest)
         return M
-
-    def image_to_top_perspective(self, image: np.ndarray) -> np.ndarray:
-        """
-        Transforms an image from the original perspective to the top perspective.
-
-        Args:
-            image (np.ndarray): The image to be transformed.
-
-        Returns:
-            np.ndarray: The transformed image.
-        """
-        height, width = image.shape[:2]
-        M = self._get_projective_transform_matrix()
-        return cv2.warpPerspective(image, M, (width, height), borderValue=(255, 255, 255))
 
     def _downscale_df(self, df):
         df = df.copy()
@@ -110,37 +156,3 @@ class CoordinateTransform:
             XY_transformed[i] = np.append(id_count[i], XY_transformed[i])
 
         return XY_transformed
-
-    def transform_coordinates(self, track_boxes_df: pd.DataFrame = None,
-                              file_save_name: str = 'track_df_new_coords.csv') -> pd.DataFrame:
-        # transform pos person -> pos trans
-        # 25 FPS
-        M = self._get_projective_transform_matrix()
-
-        if track_boxes_df is None:
-            track_boxes_df = pd.read_csv(self.default_tracking_players_path)
-
-        shift = M @ np.array([0, 0, 1])
-        track_boxes_df = self._downscale_df(track_boxes_df)
-        tracking_coords_transformed = []
-
-        for i in range(1, track_boxes_df["frame"].max() + 1):
-            if i > self.MAX_FRAMES:
-                logger.info(f"Max frames reached. Stopping: {self.MAX_FRAMES}")
-                break
-            logger.info(f"Transforming frame {i}...")
-            df = track_boxes_df[track_boxes_df["frame"] == i]
-            tracking_coords_transformed += self._get_position_transformed(df, transformation_matrix=M)
-
-        transformed_coords = pd.DataFrame(tracking_coords_transformed, columns=['id', 'x', 'y', 'coef'])
-        transformed_coords['frame'] = track_boxes_df['frame']
-        transformed_coords['x'] -= shift[0]
-        transformed_coords['y'] -= shift[1]
-        transformed_coords = transformed_coords[['frame', 'id', 'x', 'y', 'coef']]
-        transformed_coords.to_csv(file_save_name)
-        return transformed_coords
-
-    def get_top_view_center(self) -> np.ndarray:
-        M = self._get_projective_transform_matrix()
-        center = M @ np.array(self.center_orig_perspective + [1])
-        return center
