b91afed81c39bff54ff11003bb5408b9e8bcf132
Daniel Timchenko <mikecartibeats@gmail.com>
2024-02-28 20:31:12 +0200

    labels

--- coordinate_transform/camera.py
+++ coordinate_transform/camera.py
@@ -1,15 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from typing import List, Tuple
 from sympy import Point3D, Plane as sPlane, Line3D
+from typing import Tuple
 
 # Define type aliases for better readability
-Point = Tuple[float, float, float]
-Plane = List[Point]
+Point = np.ndarray
 Rectangle = Tuple[Point, Point, Point, Point]
 
 
-def calculate_fov_rectangle(cam_coords: Point, camera_angles: Tuple[float, float, float], dist: int) -> Rectangle:
+def calculate_fov_rectangle(cam_coords: Point, camera_angles: np.ndarray, dist: float) -> Rectangle:
     """
     This function calculates the fov rectangle for a given camera coordinates, angle and plane coordinates.
 
@@ -20,13 +19,11 @@ def calculate_fov_rectangle(cam_coords: Point, camera_angles: Tuple[float, float
     :param cam_coords: Coordinates of the camera
     :type cam_coords: Point
     :param camera_angles: Camera angles in plane's coordinate system
-    :type camera_angles: Tuple[float, float, float]
+    :type camera_angles: np.ndarray
     :rtype: Rectangle
     """
     # Convert camera angles from degrees to radians
     theta, phi, psi = np.radians(camera_angles)
-
-    # x_cam, y_cam, z_cam = camera_coords
 
     # Calculate the direction vector of the camera
     direction_vector = np.array([
@@ -35,52 +32,47 @@ def calculate_fov_rectangle(cam_coords: Point, camera_angles: Tuple[float, float
         -np.sin(phi)
     ])
 
+    center = cam_coords - dist * direction_vector
+
     # Calculate vectors orthogonal to the direction vector
     u_vector = np.array([-np.sin(theta), np.cos(theta), 0])
-    v_vector = np.cross(direction_vector, u_vector)
+    v_vector = np.cross(u_vector, direction_vector)
 
     # Calculate half FOV angles in radians
     half_fov_horizontal = np.radians(fov[0] / 2)
     half_fov_vertical = np.radians(fov[1] / 2)
 
     # Calculate the vectors representing the corners of the rectangle
-    center = cam_coords + dist * direction_vector
     top_left_corner = center - half_fov_horizontal * u_vector + half_fov_vertical * v_vector
     top_right_corner = center + half_fov_horizontal * u_vector + half_fov_vertical * v_vector
     bottom_left_corner = center - half_fov_horizontal * u_vector - half_fov_vertical * v_vector
     bottom_right_corner = center + half_fov_horizontal * u_vector - half_fov_vertical * v_vector
 
-    return [top_left_corner, top_right_corner, bottom_right_corner, bottom_left_corner]
+    return top_left_corner, top_right_corner, bottom_right_corner, bottom_left_corner
 
-# rewrite plagiarism
-def plot_fov_rectangle(camera_coords: Point, camera_angles: Tuple[float, float, float], fov: Tuple[float, float],
-                       near_distance: float, plane: Plane) -> None:
+
+def plot_fov_rectangle(camera_coords: Point, camera_angles: np.ndarray, fov: Tuple[float, float],
+                       near_distance: float, plane: Point) -> None:
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
     # Plot camera point
-    ax.scatter(camera_coords[0], camera_coords[1], camera_coords[2], color='red', label='Camera')
+    ax.scatter(camera_coords[0], camera_coords[1], camera_coords[2], color='red', label='Оптический центр линзы')
 
     # Calculate and plot FOV rectangle
     fov_rectangle_corners = calculate_fov_rectangle(camera_coords, camera_angles, near_distance)
-    x_vals = [corner[0] for corner in fov_rectangle_corners]
-    y_vals = [corner[1] for corner in fov_rectangle_corners]
-    z_vals = [corner[2] for corner in fov_rectangle_corners]
+    x_vals, y_vals, z_vals = zip(*fov_rectangle_corners)
     ax.plot([x_vals[0], x_vals[1], x_vals[2], x_vals[3], x_vals[0]],
             [y_vals[0], y_vals[1], y_vals[2], y_vals[3], y_vals[0]],
-            [z_vals[0], z_vals[1], z_vals[2], z_vals[3], z_vals[0]], color='blue')
+            [z_vals[0], z_vals[1], z_vals[2], z_vals[3], z_vals[0]], color='blue',
+            label="Фокальная плоскость камеры")
 
     # Plot z=0 plane
-    plane_corners = np.array(plane)
-    ax.plot([plane_corners[0][0], plane_corners[1][0], plane_corners[2][0], plane_corners[3][0], plane_corners[0][0]],
-            [plane_corners[0][1], plane_corners[1][1], plane_corners[2][1], plane_corners[3][1], plane_corners[0][1]],
-            [plane_corners[0][2], plane_corners[1][2], plane_corners[2][2], plane_corners[3][2], plane_corners[0][2]],
-            color='green', alpha=0.5)
+    ax.plot([plane[0][0], plane[1][0], plane[2][0], plane[3][0], plane[0][0]],
+            [plane[0][1], plane[1][1], plane[2][1], plane[3][1], plane[0][1]],
+            [plane[0][2], plane[1][2], plane[2][2], plane[3][2], plane[0][2]],
+            color='green', alpha=0.5, label="Наблюдаемая плоскость")
 
-    # Set tick frequency
-    # ax.set_xticks(np.arange(0, 11, 2))
-    # ax.set_yticks(np.arange(0, 11, 2))
-    # ax.set_zticks(np.arange(0, 11, 2))
     ax.set_aspect('equal')
 
     ax.set_xlabel('X')
@@ -91,45 +83,41 @@ def plot_fov_rectangle(camera_coords: Point, camera_angles: Tuple[float, float, 
 
     plt.show()
 
-# rewrite plagiarism
-def find_intersection(plane_points: List[Point], ) -> np.ndarray:
-    # plane Points
-    a1 = Point3D(*plane_points[0])
-    a2 = Point3D(*plane_points[1])
-    a3 = Point3D(*plane_points[2])
-    # line Points
-    p0 = Point3D(0, 3, 1)  # point in line
-    v0 = [0, 1, 1]  # line direction as vector
 
-    # create plane and line
-    plane = sPlane(a1, a2, a3)
-
-    line = Line3D(p0, direction_ratio=v0)
-
-    print(f"plane equation: {plane.equation()}")
-    print(f"line equation: {line.equation()}")
-
-    # find intersection:
-
-    intr = plane.intersection(line)
-
-    intersection = np.array(intr[0], dtype=float)
-    print(f"intersection: {intersection}")
-    return intersection
-
-
-def calculate_projection_corners(sensor_coords: Plane, camera_coords: Point, camera_angles: Tuple[float, float, float],
-                                 plane_coords: Plane) -> List[Point]:
-    pass
+# def find_intersection(plane_points: Point) -> np.ndarray:
+#     # plane Points
+#     a1 = Point3D(*plane_points[0])
+#     a2 = Point3D(*plane_points[1])
+#     a3 = Point3D(*plane_points[2])
+#     # line Points
+#     p0 = Point3D(0, 3, 1)  # point in line
+#     v0 = [0, 1, 1]  # line direction as vector
+#
+#     # create plane and line
+#     plane = sPlane(a1, a2, a3)
+#
+#     line = Line3D(p0, direction_ratio=v0)
+#
+#     print(f"plane equation: {plane.equation()}")
+#     print(f"line equation: {line.equation()}")
+#
+#     # find intersection:
+#     intr = plane.intersection(line)
+#
+#     intersection = np.array(intr[0], dtype=float)
+#     print(f"intersection: {intersection}")
+#     return intersection
 
 
 # Example usage:
-camera_coords = (2.5, 2.5, 5)
-camera_angles = (0, 90, 0)  # Theta, Phi, Psi in degrees
-fov = (60, 45)  # Horizontal and vertical FOV in degrees
-near_distance = 1  # Distance from the camera to the rectangle
-height = 5
-width = 5
-plane = [(0, 0, 0), (height, 0, 0), (height, width, 0), (0, width, 0)]  # Plane with z = 0
+length = 50
+width = 25
+
+camera_coords = np.array([length / 2, -5, 5])
+camera_angles = np.array([90, 20, 0])  # Theta, Phi, Psi in degrees
+# fov = (60, 45)  # Horizontal and vertical FOV in degrees
+fov = (180, 90)  # Horizontal and vertical FOV in degrees
+near_distance = 2  # Distance from the camera to the rectangle
+plane = np.array([(0, 0, 0), (length, 0, 0), (length, width, 0), (0, width, 0)])  # Plane with z = 0
 
 plot_fov_rectangle(camera_coords, camera_angles, fov, near_distance, plane)
