d897d24a8eddfe517244bd84667c79b54e9f56e3
Daniel Timchenko <mikecartibeats@gmail.com>
2024-05-04 17:43:35 -0400

    refactor

--- simulation/simulation_v2.py
+++ simulation/simulation_v2.py
@@ -7,16 +7,18 @@ from typing import Tuple, List
 
 with open("simulation_config.yaml", "r") as config_file:
     sim_config = yaml.safe_load(config_file)
-    
+
 grid_height = sim_config["height"]
 grid_width = sim_config["width"]
 sim_length_sec = sim_config["simulation_length_seconds"]
 framerate = sim_config["framerate"]
 
+
 class Grid:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+
 
 class Entity:
     def __init__(self, position, grid):
@@ -29,14 +31,16 @@ class Entity:
         x, y = self.current_position[0] + direction[0], self.current_position[1] + direction[1]
         self.current_position = (max(0, min(x, self.grid.width)), max(0, min(y, self.grid.height)))
 
+
 class Ball(Entity):
     def __init__(self, position, grid):
         super().__init__(position, grid)
         self.controlled_by = None
         self.target = None
 
+
 class Player(Entity):
-    #игрок должен бежать в свою зону, если не в ней
+    # игрок должен бежать в свою зону, если не в ней
     def __init__(self, team_name, team, player_id, default_position, grid):
         super().__init__(default_position, grid)
         self.team_name = team_name
@@ -60,14 +64,17 @@ class Player(Entity):
         return self.zone[0] <= int(ball[0]) <= self.zone[1]
 
     def is_closest_to_ball(self, ball):
-        return self == self.team.players[np.argmin([np.linalg.norm(np.array(player.current_position) - np.array(ball)) for player in self.team.players])]
+        return self == self.team.players[np.argmin(
+            [np.linalg.norm(np.array(player.current_position) - np.array(ball)) for player in self.team.players])]
 
     def get_closest_opposing_player(self, opposing_team):
         return opposing_team.players[np.argmin([np.linalg.norm(np.array(self.current_position) - np.array(pos)) \
-                for pos in [player.current_position for player in opposing_team.players]])].current_position
+                                                for pos in [player.current_position for player in
+                                                            opposing_team.players]])].current_position
 
     def get_target_position(self, ball):
-        return ball if self.is_ball_in_zone(ball) else (np.random.randint(self.zone[0], self.zone[1]), np.random.randint(0, self.grid.height))
+        return ball if self.is_ball_in_zone(ball) else (
+        np.random.randint(self.zone[0], self.zone[1]), np.random.randint(0, self.grid.height))
 
 
 class Goalkeeper(Player):
@@ -76,8 +83,10 @@ class Goalkeeper(Player):
         self.zone = zone
 
     def move(self, ball, target):
-        self.target = (np.random.randint(self.zone[0][0], self.zone[0][1]), np.random.randint(self.zone[1][0], self.zone[1][1]))
+        self.target = (
+        np.random.randint(self.zone[0][0], self.zone[0][1]), np.random.randint(self.zone[1][0], self.zone[1][1]))
         super().move(self.target)
+
 
 class OffencePlayer(Player):
     def __init__(self, team_name, team, player_id, default_position, grid, zone):
@@ -98,6 +107,7 @@ class OffencePlayer(Player):
                 self.target = (np.random.randint(self.zone[0], self.zone[1]), np.random.randint(0, self.grid.height))
         super().move(self.target)
 
+
 class CenterPlayer(Player):
     def __init__(self, team_name, team, player_id, default_position, grid, zone):
         super().__init__(team_name, team, player_id, default_position, grid)
@@ -109,6 +119,7 @@ class CenterPlayer(Player):
         else:
             self.target = self.get_target_position(ball)
         super().move(self.target)
+
 
 class DefencePlayer(Player):
     def __init__(self, team_name, team, player_id, default_position, grid, zone):
@@ -122,8 +133,9 @@ class DefencePlayer(Player):
             self.target = self.get_target_position(ball)
         super().move(self.target)
 
+
 class Team:
-    def __init__(self, name: str, grid, formation: List[int], opposing_team = None):
+    def __init__(self, name: str, grid, formation: List[int], opposing_team=None):
         self.name = name
         self.opposing_team = opposing_team
         self.players = []
@@ -158,12 +170,13 @@ class Team:
     def update_mode(self):
         self.mode = "offensive" if any(player.has_ball_control for player in self.players) else "defensive"
 
+
 class SoccerMatch:
     def __init__(self, grid, team1, formation1: List[int], team2, formation2: List[int]):
         self.grid = grid
         self.team1, self.team2 = Team(team1, self.grid, formation1), Team(team2, self.grid, formation2)
         self.team1.opposing_team, self.team2.opposing_team = self.team2, self.team1
-        self.ball = Ball([50,50], self.grid)
+        self.ball = Ball([50, 50], self.grid)
 
     def simulate(self):
         data = []
@@ -176,16 +189,18 @@ class SoccerMatch:
                 self.ball.move_towards_target(self.grid, self.ball.target.current_position)
             for team in [self.team1, self.team2]:
                 for player in team.players:
-                    data.append([1, i, i*0.04, team.name, player.player_id, *player.current_position, *self.ball.current_position])
+                    data.append([1, i, i * 0.04, team.name, player.player_id, *player.current_position,
+                                 *self.ball.current_position])
         df = pd.DataFrame(data, columns=['Period', 'Frame', 'Time [s]', 'Team', 'Player', 'X', 'Y', 'Ball_x', 'Ball_y'])
 
-        #df = df.pivot(index=['Period', 'Frame', 'Time [s]', 'Ball_x', 'Ball_y'], columns=['Team', 'Player'])
-        #df.columns = [f'Player{player_id+1}_{coord}' for player_id in range(22) for coord in ['x', 'y']]
-        #df.reset_index(inplace=True)
-        #columns = list(df.columns)
-        #columns_ball_reordered = [col for col in columns if col not in ['Ball_x', 'Ball_y']] + ['Ball_x', 'Ball_y']
-        #df = df[columns_ball_reordered]
+        # df = df.pivot(index=['Period', 'Frame', 'Time [s]', 'Ball_x', 'Ball_y'], columns=['Team', 'Player'])
+        # df.columns = [f'Player{player_id+1}_{coord}' for player_id in range(22) for coord in ['x', 'y']]
+        # df.reset_index(inplace=True)
+        # columns = list(df.columns)
+        # columns_ball_reordered = [col for col in columns if col not in ['Ball_x', 'Ball_y']] + ['Ball_x', 'Ball_y']
+        # df = df[columns_ball_reordered]
         return df
+
 
 df_formations = pd.read_csv('formation442.csv', header=None, names=['area_x', 'area_y', 'x', 'y'], sep=',')
 df_team_A = df_formations.iloc[:11]
@@ -196,5 +211,3 @@ field = Grid(grid_width, grid_height)
 match = SoccerMatch(field, 'Team A', formation1, 'Team B', formation2)
 df = match.simulate()
 df.to_csv('soccer_sim.csv', index=False)
-
-
