63a8f5b10d026f47405aaf9e2d21eb93123b2bb9
tkhaib <trossmanov@mail.ru>
2024-06-01 09:37:42 +0300

    simualtion results

--- simulation/evaluate.py
+++ simulation/evaluate.py
@@ -8,7 +8,10 @@ df_sim = pd.read_csv('soccer_sim.csv')
 df_game = pd.read_csv('heatmaps/clear_data.csv')
 df_game = df_game[df_game['Period'] == 1]
 
-def calculate_player_stats_sim(player_id: int, team: str, dt: float):
+def calculate_player_stats_sim(player_id: int, team: str, dt: float, sim_num: int):
+    filename = f'soccer_simulations/soccer_sim_{sim_num}.csv'
+
+    df_sim = pd.read_csv(filename)
     player_df = df_sim[(df_sim['Player'] == player_id) & (df_sim['Team'] == team)]
 
     player_df['Vx'] = player_df['X'].diff() / dt
@@ -25,7 +28,6 @@ def calculate_player_stats_sim(player_id: int, team: str, dt: float):
     avg_direction_vector = [player_df['Direction_X'].mean(), player_df['Direction_Y'].mean()]
 
     return mean_speed, speed_variance, avg_direction_vector
-
 
 def calculate_player_stats_game(player_id: str, dt: float):
     player_df = df_game[['Time [s]', player_id + '_x', player_id + '_y']]
@@ -45,24 +47,39 @@ def calculate_player_stats_game(player_id: str, dt: float):
 
     return mean_speed, speed_variance, avg_direction_vector
 
-def calculate_all_players_stats(dt: float):
+def calculate_all_players_stats(dt: float, num_sims: int):
     player_teams = df_sim[['Player', 'Team']].drop_duplicates().values
+    avg_stats_sim = []
 
-    stats_df_sim = pd.DataFrame(columns=['Player_id', 'Team', 'Mean', 'Variance', 'Direction'])
+    avg_stats_sim = pd.DataFrame(columns=['Player_id', 'Team', 'Mean', 'Variance', 'Direction'])
     stats_df_game = pd.DataFrame(columns=['Player_id', 'Mean', 'Variance', 'Direction'])
 
     for player_id, team in player_teams:
-        mean_speed_sim, speed_variance_sim, avg_direction_vector_sim = calculate_player_stats_sim(player_id, team, dt)
+        mean_speed_sum = 0
+        speed_variance_sum = 0
+        avg_direction_vector_sum = np.zeros(2)
+        
+        for sim_number in range(1, num_sims + 1):
+            mean_speed_sim, speed_variance_sim, avg_direction_vector_sim = calculate_player_stats_sim(player_id, team, dt, sim_number)
+            
+            mean_speed_sum += mean_speed_sim
+            speed_variance_sum += speed_variance_sim
+            avg_direction_vector_sum += np.array(avg_direction_vector_sim)
+        
+        num_sims_float = float(num_sims)
+        avg_mean_speed = mean_speed_sum / num_sims_float
+        avg_speed_variance = speed_variance_sum / num_sims_float
+        avg_direction_vector = avg_direction_vector_sum / num_sims_float
 
         new_row = pd.DataFrame({
-            'Player_id': [player_id],
+            'Player_id': [f"Player{player_id + 1}"],
             'Team': [team],
             'Mean': [mean_speed_sim],
             'Variance': [speed_variance_sim],
             'Direction': [avg_direction_vector_sim]
         })
 
-        stats_df_sim = pd.concat([stats_df_sim, new_row], ignore_index=True) 
+        avg_stats_sim = pd.concat([avg_stats_sim, new_row], ignore_index=True) 
 
     for i in range(1, 23):
         player_id_game = 'Player' + str(i)
@@ -77,10 +94,15 @@ def calculate_all_players_stats(dt: float):
 
         stats_df_game = pd.concat([stats_df_game, new_row], ignore_index=True)
 
-    return stats_df_sim, stats_df_game
+    return avg_stats_sim, stats_df_game
 
 dt = 0.04
-stats_df_sim, stats_df_game = calculate_all_players_stats(dt)  
+num_sims = 20
+stats_df_sim, stats_df_game = calculate_all_players_stats(dt, num_sims)  
+stats_df_sim['Player_id'] = stats_df_game['Player_id']
+stats_df_sim.drop(['Team'], axis=1, inplace=True)
+stats_df_sim.to_csv('results/stats_sim.csv', index=False)
+stats_df_game.to_csv('results/stats_game.csv', index=False)
 print("Simulation Stats:\n", stats_df_sim)
 print("Game Stats:\n", stats_df_game)
 
